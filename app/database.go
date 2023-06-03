d699ed2c66b70516fbcb0e51a7617e2a5eb0a07f
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-06-03 21:09:41 +0700

    Update database.go

--- app/database.go
+++ app/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:kozato321@tcp(localhost:3306)/go_database_migration")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go_database_migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
